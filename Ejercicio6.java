fe5e26bac2f044f6f42105b07ae639ae1a4f188a
Oliver <oliversanchezalaminoscolegiolagomar.es>
2025-12-04 20:20:23 +0100

    Conflicto resuelto

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,5 +1,5 @@
 public class Ejercicio4 {
 public static void main(String[] args) {
-System.out.println("Versión local y remota del Ejercicio6");
+System.out.println("Versión local y remota del Ejercicio6 ");
 }
 }
